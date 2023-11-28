858d29fff84b497312ee6658ec6926ba729530d4
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-11-28 12:43:26 +0600

    Update uptime.ts

--- src/uptime.ts
+++ src/uptime.ts
@@ -1,6 +1,7 @@
+import { UptimeFormat } from "./type"
 const start = performance.now()
 
-export function uptime(): string {
+export function uptime(type?: UptimeFormat | boolean | "long" | "short"): string {
     const duration = Number((performance.now() - start).toFixed())
     const milliseconds = parseInt(String((duration % 1000) / 100))
     const seconds = Math.floor((duration / 1000) % 60)
@@ -9,21 +10,38 @@ export function uptime(): string {
     const days = Math.floor(duration / (1000 * 60 * 60 * 24))
 
     let result = ""
-    if (days > 0) {
-        result += `${days} day${days > 1 ? "s" : ""} `
+    if (type === UptimeFormat.Short || type === "short" || type === true) {
+        if (days > 0) {
+            result += `${days}d `
+        }
+        if (hours > 0) {
+            result += `${hours}h `
+        }
+        if (minutes > 0) {
+            result += `${minutes}m `
+        }
+        if (seconds > 0) {
+            result += `${seconds}s `
+        }
+        if (milliseconds > 0) {
+            result += `${milliseconds}ms`
+        }
+    } else {
+        if (days > 0) {
+            result += `${days} day${days > 1 ? "s" : ""} `
+        }
+        if (hours > 0) {
+            result += `${hours} hour${hours > 1 ? "s" : ""} `
+        }
+        if (minutes > 0) {
+            result += `${minutes} minute${minutes > 1 ? "s" : ""} `
+        }
+        if (seconds > 0) {
+            result += `${seconds} second${seconds > 1 ? "s" : ""} `
+        }
+        if (milliseconds > 0) {
+            result += `${milliseconds} millisecond${milliseconds > 1 ? "s" : ""}`
+        }
     }
-    if (hours > 0) {
-        result += `${hours} hour${hours > 1 ? "s" : ""} `
-    }
-    if (minutes > 0) {
-        result += `${minutes} minute${minutes > 1 ? "s" : ""} `
-    }
-    if (seconds > 0) {
-        result += `${seconds} second${seconds > 1 ? "s" : ""} `
-    }
-    if (milliseconds > 0) {
-        result += `${milliseconds} millisecond${milliseconds > 1 ? "s" : ""} `
-    }
-
-    return result.trim()
+    return result
 }
